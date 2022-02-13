82483589e7b24af115178a2bfd596245baa230e4
Koko-Dev <koko.webdev@gmail.com>
2022-02-12 20:35:04 -0500

    Add classNames to ExpenseItem.js component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,15 @@
 function ExpenseItem() {
 	return (
-		<h2>Expense item!</h2>
+		<div className="expense-item">
+			<div>
+				<div>February 13, 2022</div>
+				<div className="expense-item__description">
+					<h2>Car Insurance</h2>
+					<div className="expense-item__price">$249.67</div>
+				</div>
+			</div>
+		</div>
 	)
-
 }
 
 export default ExpenseItem;

4b80e74ce11765506c0e5f29a0806565c7afb0be
Koko-Dev <koko.webdev@gmail.com>
2022-02-12 21:22:47 -0500

    Refactor ExpenseItem.js and add styling in ExpenseItem.css file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
 	return (
 		<div className="expense-item">
-			<div>
-				<div>February 13, 2022</div>
-				<div className="expense-item__description">
-					<h2>Car Insurance</h2>
-					<div className="expense-item__price">$249.67</div>
-				</div>
+			<div>February 13, 2022</div>
+			<div className="expense-item__description">
+				<h2>Car Insurance</h2>
+				<div className="expense-item__price">$294.67</div>
 			</div>
 		</div>
 	)

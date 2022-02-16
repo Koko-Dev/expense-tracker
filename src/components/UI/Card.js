8ac46205da1848f695bd58d6bfe566456e707631
Koko-Dev <koko.webdev@gmail.com>
2022-02-16 11:16:52 -0500

    Change all functions in JS files to Arrow Functions

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
 
-function Card(props) {
+const card = (props) => {
 	const classes = 'card ' + props.className;
 	return <div className={classes}>{props.children}</div>;
 }

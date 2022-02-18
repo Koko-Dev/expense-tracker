48e561851a3689750711104e5094efcf35526794
Koko-Dev <koko.webdev@gmail.com>
2022-02-17 21:24:25 -0500

    Capitalize const Card

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
 
-const card = (props) => {
+const Card = (props) => {
 	const classes = 'card ' + props.className;
 	return <div className={classes}>{props.children}</div>;
 }

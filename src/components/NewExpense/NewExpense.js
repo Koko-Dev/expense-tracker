a2a2eb92f316215d3cb84ea1c2bfacfab36fad48
Koko-Dev <koko.webdev@gmail.com>
2022-02-19 08:51:38 -0500

    Add new directory within components/ called NewExpense; add NewExpense.js and NewExpense.css;  in NewExpense.js, create a component with a className of new-expense with an instance of ExpenseItem in its body;  recall that ExpenseItem displays the item name, description and price and a button which calls for the reevaluation of button props.title through useState

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExpenseItem from "../Expenses/ExpenseItem";
+
+const NewExpense = () => {
+	return (
+		// todo: Add capability to add user input
+		<div className="new-expense">
+			<ExpenseItem />
+		</div>
+	)
+}
+
+export default NewExpense;

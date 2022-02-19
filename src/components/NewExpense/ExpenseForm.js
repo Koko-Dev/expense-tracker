f09df605e897cde52d51fea12cf4ddc693a250ba
Koko-Dev <koko.webdev@gmail.com>
2022-02-19 10:48:00 -0500

    Complete styling of ExpenseForm and NewExpense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,10 +5,10 @@ import './ExpenseForm.css';
 const ExpenseForm = () => {
 	return (
 		<form>
-			<div className="new-expense__controls">
-				<div className="new-expense__control">
+			<div className='new-expense__controls'>
+				<div className='new-expense__control'>
 					<label>Title</label>
-					<input type="text" />
+					<input type='text' />
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
@@ -16,14 +16,14 @@ const ExpenseForm = () => {
 				</div>
 				<div className='new-expense__control'>
 					<label>Date</label>
-					<input type='date' min='2020-01-01' max='2022-12-31'/>
-				</div>
-				<div className='new-expense__actions'>
-					<button type='submit'>Add Expense</button>
+					<input type='date' min='2020-01-01' max='2023-12-31' />
 				</div>
 			</div>
+			<div className='new-expense__actions'>
+				<button type='submit'>Add Expense</button>
+			</div>
 		</form>
-	)
-}
+	);
+};
 
 export default ExpenseForm;

22be85e2cfbf3606cebd235de5512dba83f42806
Koko-Dev <koko.webdev@gmail.com>
2022-02-19 07:53:49 -0500

    Add testing of useState to ExpenseItem component to test that useState for props.title is only being evaluated on a per instance basis after initial npm start

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,15 @@ import './ExpenseItem.css';
 const ExpenseItem = (props) => {
 	const [title, setTitle] = useState(props.title);
 
+	// Note: This will be called whenever the ExpenseItem Component
+	//  is being executed
+	// note: Upon npm start, this console log will be called 4 times.
+	//  This is the case becasue ExpenseItem is being used four times
+	//  in Expenses.js.
+	// note: When the button is clicked, it will be called only once
+	//  because a new useState is being called for reevaluation
+	console.log('ExpenseItem evaluated by React');
+
 	const clickHandler = () => {
 		setTitle('Updated');
 		console.log(title);

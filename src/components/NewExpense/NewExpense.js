c0da98df63aab386db0374d01e43cd30fa1ed66f
Koko-Dev <koko.webdev@gmail.com>
2022-02-19 09:32:42 -0500

    Add ExpenseForm.js and ExpenseForm.css to the NewExpense directory; in NewExpense.js, remove call to ExpenseItem component and instead add ExpenseForm component

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './NewExpense.css'
 import ExpenseItem from "../Expenses/ExpenseItem";
 
+// Goal: return a form for our inputs. Form will be in other component
 const NewExpense = () => {
 	return (
-		// todo: Add capability to add user input
+		// todo: Add capability to add user input.
 		<div className="new-expense">
-			<ExpenseItem />
+			<ExpenseForm />
 		</div>
 	)
 }

55cf08283459e75f0478b065b53efe26bf233efb
Koko-Dev <koko.webdev@gmail.com>
2022-02-24 13:30:07 -0500

    In ExpenseForm.js component, add an example of alternative #4 in titleChangeHandler, but go back to first alternative where we call multiple useState(), one for each input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,44 +6,58 @@ const ExpenseForm = () => {
 	// Important: Store State => The second variable is a function which
 	//  will store user entry in our state; this is done in event handler
 	//  where the user input becomes the argument for setEntered___
-	/*const [enteredTitle, setEnteredTitle] = useState('');
+	const [enteredTitle, setEnteredTitle] = useState('');
 	const [enteredAmount, setEnteredAmount] = useState('');
-	const [enteredDate, setEnteredDate] = useState('');*/
+	const [enteredDate, setEnteredDate] = useState('');
 
 	// alternate 2 and 3
-	const [userInput, setUserInput] = useState({
+	/*const [userInput, setUserInput] = useState({
 		enteredTitle: '',
 		enteredAmount: '',
 		enteredDate: ''
-	})
+	})*/
 
 	const titleChangeHandler = (event) => {
-		// setEnteredTitle(event.target.value);
+		setEnteredTitle(event.target.value);
 		// alternate 2
-		setUserInput({
+		/*setUserInput({
 			enteredTitle: event.target.value,
 			enteredAmount: '',
 			enteredDate: ''
-		})
+		})*/
 
-		// alternate 3
+		// alternate 3 -- NOT RECOMMENDED
 		/*setUserInput({
 			...userInput,
 			enteredTitle: event.target.value
 		})*/
+
+		// NOTE:  alternative to 3
+		// alternate 4
+		// Important:  Whenever you update state and you depend on the previous state,
+		//  as in alternative 3 or when using a counter, you should use an alternative
+		//  form like so. This will receive the previous snapshot in argument prevState
+		// note: This is safer than alternative 3 because it doesn't rely on react's
+		//  scheduled updates since REACT doesn't perform the updates instantly.
+		//  Using alternative 3 it is possible to receive an old snapshot instead
+		//  of a current one.
+		// note: prevState:{enteredAmount:string, enteredDate:string, enteredTitle:string}
+		/*setUserInput((prevState) => {
+			return {...prevState, enteredTitle: event.target.value};
+		})*/
 	};
 
 	const amountChangeHandler = (event) => {
-		// setEnteredAmount(event.target.value);
+		setEnteredAmount(event.target.value);
 
 		// alternate 2
-		setUserInput({
+		/*setUserInput({
 			enteredAmount: event.target.value,
 			enteredTitle: '',
 			enteredDate: ''
-		})
+		})*/
 
-		// alternate 3
+		// alternate 3 -- NOT RECOMMENDED
 		/*setUserInput({
 			...userInput,
 			enteredAmount: event.target.value,
@@ -51,15 +65,15 @@ const ExpenseForm = () => {
 	};
 
 	const dateChangeHandler = (event) => {
-		// setEnteredDate(event.target.value);
+		setEnteredDate(event.target.value);
 		// alternate 2
-		setUserInput({
+		/*setUserInput({
 			enteredDate: event.target.value,
 			enteredAmount: '',
 			enteredTitle: ''
-		})
+		})*/
 
-		// alternate 3
+		// alternate 3 -- NOT RECOMMENDED
 		/*setUserInput({
 			...userInput,
 			enteredDate: event.target.value

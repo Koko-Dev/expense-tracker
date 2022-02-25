211155755f4b229c2bb30d090d7c164f10cdae0d
Koko-Dev <koko.webdev@gmail.com>
2022-02-25 14:56:24 -0500

    In NewExpense.js, add prop onSaveExpenseData to <ExpenseForm /> which calls function saveExpenseDataHandler(enteredExpenseData) which receives expenseData -- all form input from client --  from ExpenseForm component; add a random id to this data and log to the console;  In order to pass expenseData, including the new id, to App.js
    Note:  The console at this point will only receive the id => // ex. output: {id: '0.07278843426987991'}
    
    const NewExpense = () => {
        const saveExpenseDataHandler = (enteredExpenseData) => {
            const expenseData = {
                ...enteredExpenseData,
                id: Math.random().toString()
            };
    
            // output: {id: '0.07278843426987991'}
            console.log(expenseData);
        }
    
        return (
            <div className="new-expense">
                <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
            </div>
        )
    }
    
    export default NewExpense;

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -7,10 +7,20 @@ import './NewExpense.css';
 // Goal: return a form for our inputs.
 //  Form will be in ExpenseForm.js Component
 const NewExpense = () => {
+	const saveExpenseDataHandler = (enteredExpenseData) => {
+		const expenseData = {
+			...enteredExpenseData,
+			id: Math.random().toString()
+		};
+
+		// output: {id: '0.07278843426987991'}
+		console.log(expenseData);
+	}
+
 	return (
 		// todo: Add capability to add user input.
 		<div className="new-expense">
-			<ExpenseForm />
+			<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
 		</div>
 	)
 }
